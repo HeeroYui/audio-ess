bb04928dad2d7213fd79f0837478c9cdd8c89ac1
Edouard DUPIN <yui.heero@gmail.com>
2015-10-14 21:21:03 +0200

    [DEV] update new lutin 0.8.0

--- lutin_audio-ess.py
+++ lutin_audio-ess.py
@@ -3,12 +3,30 @@ import lutin.module as module
 import lutin.tools as tools
 import lutin.debug as debug
 
+
+def get_type():
+	return "LIBRARY"
+
 def get_desc():
 	return "ewol sound set interface for 'small game' audio effects"
 
+def get_licence():
+	return "APACHE-2"
 
-def create(target):
-	my_module = module.Module(__file__, 'audio-ess', 'LIBRARY')
+def get_compagny_type():
+	return "com"
+
+def get_compagny_name():
+	return "atria-soft"
+
+def get_maintainer():
+	return ["Mr DUPIN Edouard <yui.heero@gmail.com>"]
+
+def get_version():
+	return [0,0,0]
+
+def create(target, module_name):
+	my_module = module.Module(__file__, module_name, get_type())
 	# System core
 	my_module.add_src_file([
 		'audio/ess/debug.cpp',
@@ -25,7 +43,6 @@ def create(target):
 		'audio/ess/music.h',
 		'audio/ess/LoadedFile.h'
 		])
-	
 	# name of the dependency
 	my_module.add_module_depend(['etk', 'audio-river', 'ogg', 'ejson'])
 	if target.name=="Android":
@@ -33,8 +50,6 @@ def create(target):
 		my_module.add_export_flag('c++', ["-pthread", "-frtti", "-fexceptions"])
 	
 	my_module.add_path(tools.get_current_path(__file__))
-	
-	# add the currrent module at the 
 	return my_module
 
 
